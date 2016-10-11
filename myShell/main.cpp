3b0f1037f6dbb4287da45605e1cb527b13132bb7
comlor <comlor@gmail.com>
2016-10-10 22:29:51 -0700

    Functionality Update
    Server Listener Thread - 100% working, accepting multiple connections
    my port, connect, list, send, exit functions all 100% working.
    myip function needs work - copy/paste from net
    terminate - not sure if even working - haven’t had chance to debug
    receive - working for receiving data, however, data is either not being
    completely sent or received.  Still need to debug.
    
    Still needs a lot of error handling.

--- myShell/main.cpp
+++ myShell/main.cpp
@@ -11,6 +11,8 @@
 #include <netdb.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <ifaddrs.h>
+#include <net/if.h>
 
 // ****************GLOBAL VARIABLES****************
 struct tdata
@@ -18,11 +20,16 @@ struct tdata
     int thread_id;
     int status;
     int server;
+    std::string listen_port;
     int time = 10;
     bool connection_waiting = false;
     bool exit_prog = false;
     std::vector<int> client_list;
-    std::vector<struct sockaddr> client_addr;
+    std::vector<std::string> client_list_ad;
+    std::vector<uint16_t> client_list_port;
+    std::vector<sockaddr_in> client_addr;
+    sockaddr_storage server_sock;
+    struct addrinfo *serv;
 };
 
 void *get_in_addr(struct sockaddr *sa)
@@ -33,17 +40,13 @@ void *get_in_addr(struct sockaddr *sa)
 }
 
 tdata list;
-#define PORT = "30000"
+#define PORT = "5001"
 #define CLIENT_IP = "192.168.1.42"
 int NUM_ARGS = 5;
 
 // ****************SERVER FUNCTIONS****************
-void my_shell(int td);
 int start_Server(std::string portNum);
 int server_Accept(int server_sock, std::vector<int> &client_list);
-int server_Send(int dest, int server, char *msg);
-char *server_Recv(int server);
-void server_Listener(int td);
 
 // ****************TERMINAL FUNCTIONS****************
 std::string read_input(void);
@@ -51,13 +54,27 @@ std::vector<std::string> parse_input(std::string &in);
 int ex_args(std::vector<std::string> args);
 void help();
 void list_connections();
+void get_myip();
+void connect_client(std::__1::string con_ip, std::__1::string con_port);
+void terminate_socket(int sock);
+void send_msg(int sock, std::__1::string msg);
+
+// ****************THREAD FUNCTIONS****************
+void my_shell(int td);
+void server_Recv(int server);
+void server_Listener(int td);
 
 // ******************MAIN FUNCTION*****************
 int main(int argc, char *argv[])
 {
     std::cout<<std::endl<<std::endl;
     list.thread_id = 1;
-    std::string port = "30001";
+    list.client_list.resize(10);
+    list.client_list.clear();
+    //list.client_list_ad.resize(10);
+    //list.client_list_ad.clear();
+    std::string port = "5001";
+    list.listen_port = port;
     int istatus = 1;
 
     int server = start_Server(port);
@@ -75,7 +92,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-// ******************THREAD FUNCTION*****************
+// ******************THREAD FUNCTIONs*****************
 void my_shell(int td)
 {
     int status = 1;
@@ -106,7 +123,6 @@ void server_Listener(int td)
         if(list.connection_waiting == true)
         {
             client_sock = server_Accept(list.server,list.client_list);
-            list.client_list.push_back(client_sock);
             list.connection_waiting = false;
         }
         else if(list.exit_prog == true)
@@ -114,10 +130,35 @@ void server_Listener(int td)
     }
 }
 
+void server_Recv(int server)
+{
+    std::cout<<"RECEVING"<<std::endl;
+    char in[255];
+    int status;
+
+    while(1)
+    {
+        memset(&in, 0, sizeof(in));
+        status = recv(server, in, sizeof(in), MSG_DONTWAIT);
+        if(status < 0)
+        {
+            fprintf(stderr, "RECV ERROR: %S\n", gai_strerror(status));
+        }
+        else if(status > 0)
+            std::cout<<"data: " << in << std::endl;
+        else if(status == 0)
+            std::cout<<"status 0"<<std::endl;
+    }
+
+    std::cout<<"data: " << in << std::endl;
+    std::cout<<"status: " << status << std::endl;
+
+}
+
+// ***************************************************************************
 // *****************************Read Console Input****************************
 std::string read_input(void)
 {
-    //std::cout<<"READ"<<std::endl;
     std::string in;
     std::getline(std::cin, in);
     std::fflush(NULL);
@@ -127,7 +168,6 @@ std::string read_input(void)
 // *********************Parse Parameters from Console Input*******************
 std::vector<std::string> parse_input(std::string &in)
 {
-    //std::cout<<"PARSE"<<std::endl;
     int num_param = 1;
 
     for(unsigned int i = 0; i < in.length(); i++)
@@ -170,7 +210,6 @@ std::vector<std::string> parse_input(std::string &in)
 // ****************************Execute Parameters*****************************
 int ex_args(std::vector<std::string> args)
 {
-    //std::cout<<"EXARG"<<std::endl;
     int num_param = NUM_ARGS;
 
     for(int i = 0; i < num_param; i++)
@@ -185,6 +224,39 @@ int ex_args(std::vector<std::string> args)
     else if(num_param == 1 && args[0] == "LIST" )
     {
         list_connections();
+        return 1;
+    }
+    else if(num_param == 1 && args[0] == "MYIP")
+    {
+        get_myip();
+        return 1;
+    }
+    else if(num_param == 1 && args[0] == "MYPORT")
+    {
+        std::cout<<"> Listening Port: " << list.listen_port << std::endl;
+        return 1;
+    }
+    else if(num_param > 1 && args[0] == "TERMINATE")
+    {
+        terminate_socket(std::stoi(args[1]));
+        return 1;
+    }
+    else if(num_param > 1 && args[0] == "CONNECT")
+    {
+        connect_client(args[1], args[2]);
+        return 1;
+    }
+    else if(num_param > 1 && args[0] == "SEND")
+    {
+        std::string msg = "";
+        if(args.size() > 3)
+        {
+            for(int i = 2; i < args.size(); i++)
+            {
+                msg += args[i];
+            }
+        }
+        send_msg(std::stoi(args[1]), msg);
         return 1;
     }
 }
@@ -215,19 +287,172 @@ void help()
 
 void list_connections()
 {
-    sockaddr_in peeraddr;
-    socklen_t size = sizeof(peeraddr);
-    in_addr inaddr;
+//    socklen_t len;
+//    struct sockaddr_storage addr;
+//    char ipstr[INET6_ADDRSTRLEN];
+//    int port;
 
-    char saddr[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &inaddr, saddr, INET_ADDRSTRLEN);
+//    len = sizeof addr;
 
-    std::cout << "inaddr: " << inaddr.s_addr << std::endl;
-    std::cout << "saddr: " << saddr << std::endl;
+//    for(int i = 0; i < list.client_addr.size(); i++)
+//    {
+//        struct sockaddr_in *s = (struct sockaddr_in *)&list.client_addr.at(i);
+//        port = ntohs(s->sin_port);
+//        inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
+//        std::cout<<"host: " << list.client_list.at(i);
+//        std::cout<<" - "<< ipstr <<std::endl;
+//        ipstr[0] = '\0';
+//    }
+
+    std::cout<<"ID\tIP\t\tPORT"<<std::endl;
 
     for(int i = 0; i < list.client_list.size(); i++)
-        std::cout<<"host: " << list.client_list.at(i) << std::endl;
+    {
+        std::cout<<list.client_list[i]<<"\t";
+        std::cout<<list.client_list_ad[i]<<"\t";
+        std::cout<<list.client_list_port[i]<<std::endl;
+    }
 }
+
+void get_myip()
+{
+    struct ifaddrs *myaddrs, *ifa;
+    void *in_addr;
+    char buf[64];
+
+    if(getifaddrs(&myaddrs) != 0)
+    {
+        std::cout<<"MYIP ERROR"<<std::endl;
+        exit(1);
+    }
+
+    for (ifa = myaddrs; ifa != NULL; ifa = ifa->ifa_next)
+    {
+        if (ifa->ifa_addr == NULL)
+            continue;
+        if (!(ifa->ifa_flags & IFF_UP))
+            continue;
+
+        switch (ifa->ifa_addr->sa_family)
+        {
+            case AF_INET:
+            {
+                struct sockaddr_in *s4 = (struct sockaddr_in *)ifa->ifa_addr;
+                in_addr = &s4->sin_addr;
+                break;
+            }
+
+            case AF_INET6:
+            {
+                struct sockaddr_in6 *s6 = (struct sockaddr_in6 *)ifa->ifa_addr;
+                in_addr = &s6->sin6_addr;
+                break;
+            }
+
+            default:
+                continue;
+        }
+
+        if (!inet_ntop(ifa->ifa_addr->sa_family, in_addr, buf, sizeof(buf)))
+        {
+            printf("%s: inet_ntop failed!\n", ifa->ifa_name);
+        }
+        else
+        {
+            printf("%s: %s\n", ifa->ifa_name, buf);
+        }
+    }
+
+    freeifaddrs(myaddrs);
+}
+
+void connect_client(std::string con_ip, std::string con_port)
+{
+    int sock, status;
+    struct addrinfo *servinfo, tcp_info;
+
+    const char *conip = con_ip.c_str();
+    const char *conport = con_port.c_str();
+
+    memset(&tcp_info, 0, sizeof(tcp_info));
+    tcp_info.ai_family = AF_INET;
+    tcp_info.ai_socktype = SOCK_STREAM;
+    tcp_info.ai_flags = AI_PASSIVE;
+
+    if((status = getaddrinfo(conip, conport, &tcp_info, &servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    sock = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    if(sock < 0)
+    {
+        printf("\nServer socket failure %m", sock);
+        exit(1);
+    }
+
+    if(connect(sock, servinfo->ai_addr, servinfo->ai_addrlen) < 0)
+    {
+        printf("\nClient connection failure %m", sock);
+        exit(1);
+    }
+    uint16_t p = std::stoi(conport);
+    std::cout<<"\nSuccessful Connection!\n";
+    list.client_list.push_back(sock);
+    list.client_list_ad.push_back(conip);
+    list.client_list_port.push_back(p);
+
+    std::thread receive_func(server_Recv, sock);
+
+    while(!receive_func.joinable()){}
+    receive_func.detach();
+}
+
+void terminate_socket(int sock)
+{
+    shutdown(sock,SHUT_RDWR);
+
+    for(int i = 0; i < list.client_list.size(); i++)
+    {
+        if(list.client_list[i] == sock)
+        {
+            list.client_list[i] = -1;
+            list.client_list_ad[i] = "";
+            list.client_list_port[i] = 0;
+        }
+    }
+
+    shutdown(sock,2);
+}
+
+void send_msg(int sock, std::string msg)
+{
+    int client_sock, status;
+
+    char *out = (char*)malloc(255 * sizeof(char));
+    memset(&out, 0, sizeof(out));
+    out = "Hello!";
+
+    status = send(sock, msg.c_str(), strlen(out), 0);
+    if(status < 0)
+    {
+        fprintf(stderr,"> SEND ERROR: %s\n",gai_strerror(status));
+        std::cout<<std::endl;
+    }
+
+//    int status = send(sock, &msg, std::strlen(msg.c_str()), 0);
+
+//    if(status < 0)
+//    {
+//        fprintf(stderr,"> SEND ERROR: %s\n",gai_strerror(status));
+//        std::cout<<std::endl;
+//    }
+}
+
+
+// ***************************************************************************
+// ******************************Server Functions*****************************
 
 int start_Server(std::string portNum)
 {
@@ -265,7 +490,7 @@ int start_Server(std::string portNum)
             break;                  /* Success */
         }
 
-        close(server_sock);
+        //close(server_sock);
     }
 
     if (p == NULL) {               /* No address succeeded */
@@ -273,19 +498,40 @@ int start_Server(std::string portNum)
         exit(EXIT_FAILURE);
     }
 
-    freeaddrinfo(servinfo);           /* No longer needed */
+    list.serv = servinfo;
+    //freeaddrinfo(servinfo);           /* No longer needed */
 
     return server_sock;
 }
 
 int server_Accept(int server_sock, std::vector<int> &client_list)
 {
-    struct sockaddr_storage client;
+    struct sockaddr_in client;
     int client_sock, status;
+    char ip_string[INET6_ADDRSTRLEN];
     socklen_t len = sizeof(client);
 
-    client_sock = accept(server_sock, (struct sockaddr*)&client, &len);
-    list.client_list.push_back(client_sock);
+    client_sock = accept(server_sock, (struct sockaddr *) &client, &len);
+
+    inet_ntop(AF_INET, (struct sockaddr_in *)&client.sin_addr, ip_string, sizeof ip_string);
+
+    if (getsockname(client_sock, (struct sockaddr *)&client, &len) == -1)
+        std::cout<<"PORT NUMBER ERROR"<<std::endl;
+    else
+        printf("port number %d\n", ntohs(client.sin_port));
+
+    std::cout<<"connection ip: " << ip_string << std::endl;
+    //std::cout<<"connection port: " << port << std::endl;
+
+    std::string port = std::to_string(ntohs(client.sin_port));
+    //sprintf(port, "%u", ntohs(client.sin_port));
+
+    connect_client(ip_string, port);
+
+    //list.client_list.push_back(client_sock);
+    //list.client_addr.push_back(client);
+    //list.client_list_ad.push_back(ip_string);
+    //list.client_list_port.push_back(ntohs(client.sin_port));
 
     char *out;
 
@@ -298,33 +544,6 @@ int server_Accept(int server_sock, std::vector<int> &client_list)
         std::cout<<std::endl;
     }
 
-    close(client_sock);
-
     return 1;
-}
-
-int server_Send(int dest, int server, char *msg)
-{
-    int status = send(dest, &*msg, strlen(msg), 0);
-
-    if(status < 0)
-    {
-        fprintf(stderr,"> SEND ERROR: %s\n",gai_strerror(status));
-        std::cout<<std::endl;
-    }
-
-    return status;
-}
-
-char* server_Recv(int server)
-{
-    char *in = new char[255];
-    int status;
-    memset(&in, 0, sizeof(in));
-    status = recv(server, in, sizeof(in), 0);
-
-    if(status < 0)
-        fprintf(stderr, "RECV ERROR: %s\n", gai_strerror(status));
-    return in;
 }
 
